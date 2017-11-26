ad71b7544f5bba0cf481d5cbee76f3804355a039
Karl Ni <karl@Karls-MacBook-Air.local>
2017-11-25 23:01:09 -0800

    Detected Cars

--- lesson_functions.py
+++ lesson_functions.py
@@ -2,6 +2,9 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 from skimage.feature import hog
+
+
+
 # Define a function to return HOG features and visualization
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
                         vis=False, feature_vec=True):
@@ -140,21 +143,6 @@ def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
             window_list.append(((startx, starty), (endx, endy)))
     # Return the list of windows
     return window_list
-
-# Define a function to draw bounding boxes
-def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
-
-    if type(img)==str:
-        imcopy = mpimg.imread(img)
-    else:
-        # Make a copy of the image
-        imcopy = np.copy(img)
-    # Iterate through the bounding boxes
-    for bbox in bboxes:
-        # Draw a rectangle given bbox coordinates
-        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
-    # Return the image copy with boxes drawn
-    return imcopy
 
 # Define a function to extract features from a single image window
 # This function is very similar to extract_features()
@@ -315,3 +303,17 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
 
     # return draw_img
     return rectangles
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+def apply_threshold(heatmap, threshold):
+    heatmap[heatmap<=threshold] = 0
+    return heatmap
--- utility.py
+++ utility.py
@@ -2,6 +2,7 @@ import numpy as np
 from IPython.display import Image
 from IPython.display import display
 import matplotlib.pylab as plt
+import matplotlib.image as mpimg
 import cv2
 
 def flatten(lists):
@@ -10,7 +11,6 @@ def flatten(lists):
     else:
         return lists
             
-
 def show_images(imnames, titles=None, rows = 1, figsize=(30,8)):
 
     images = [plt.imread(imname) for imname in flatten(imnames)]
@@ -31,20 +31,6 @@ def show_images(imnames, titles=None, rows = 1, figsize=(30,8)):
     # fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
     plt.show()
 
-def add_heat(heatmap, bbox_list):
-    # Iterate through list of bboxes
-    for box in bbox_list:
-        # Add += 1 for all pixels inside each bbox
-        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
-        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
-
-    # Return updated heatmap
-    return heatmap
-
-def apply_threshold(heatmap, threshold):
-    heatmap[heatmap<=threshold] = 0
-    return heatmap
-
 
 def draw_labeled_bboxes(img, labels, color=(1.0, 0, 0)):
     img = np.copy(img)
@@ -61,4 +47,19 @@ def draw_labeled_bboxes(img, labels, color=(1.0, 0, 0)):
         cv2.rectangle(img, bbox[0], bbox[1], color, 6)
     # Return the image
     return img
+
+# Define a function to draw bounding boxes
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+
+    if type(img)==str:
+        imcopy = mpimg.imread(img)
+    else:
+        # Make a copy of the image
+        imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
 
